58c9a9a44485511da47f5a540d96dd40680a8f9a
bibisixtynine <ilboued@gmail.com>
2023-09-19 19:48:18 +0200

    pwa mode

---
+++ sw.js
@@ -0,0 +1,24 @@
+// sw.js
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+      caches.open("hello-world-cache").then((cache) => {
+        return cache.addAll([
+            "/",
+            "/index.html",
+            "/theo.jpg",
+            "/main.js",
+            "/jysuis.js",
+            "/styles.css"
+
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
